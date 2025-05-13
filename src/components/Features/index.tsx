5acd0cbfb82d921e25a73fdd92738beb95e0a942
Nico <nico@Nicos-MacBook-Pro.local>
2025-05-12 22:06:21 -0400

    small changes

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -5,13 +5,13 @@ import featuresData from "./featuresData";
 const Features = () => {
   return (
     <>
-      <section id="features" className="py-16 md:py-20 lg:py-28">
+      <section id="features" className="py-16 md:py-20 lg:py-20">
         <div className="container">
         <SectionTitle
-          title="Everything We Offer"
+          title="Our IT Services"
           paragraph="Browse our full range of IT services, built to support your tech from the ground up."
           center
-        />
+          />
 
 
           <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
